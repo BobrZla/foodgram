3f6af207b5c9a4152caa8f481c81a00da97b4529
Dmitry <bobrzla@yandex.ru>
2024-07-04 16:53:23 +0300

    save

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
